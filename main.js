ea188b37530a22f90e3d0af0ff4325d059c8b7eb
Brian Goyvaerts <r0595245@student.thomasmore.be>
2016-10-17 21:17:51 +0200

    Met behulp van uitleg en stukken code van Peter Leriche lukt het mij ook.

--- main.js
+++ main.js
@@ -3,21 +3,30 @@
  */
 var express = require("express");
 var app = express();
-var bodyparser = require('body-parser');
+var bustenparser = require('body-parser');
 
-app.use(bodyparser.urlencoded({extended:true}));
+app.use(bustenparser.urlencoded({extended:true}));
 
 
-app.post('/som',function (request,response) {
+app.get('/',function (request,response) {
+    res.send('opgave');
+});
+
+app.get('/calc',function(request,response){
     console.dir(request.body);
-    var som = request.body.bodysom;
+    var opgave = request.body.bodyopgave;
 
-    var uitkomst = eval(request.body.bodysom);
+    var uitkomst = eval(request.body.bodyopgave);
     console.dir(uitkomst);
     response.send(String(uitkomst));
+});
 
+app.get('/calc/:opdracht',function(request,response){
+    var opdracht = request.params.opdracht;
+    var uitkomst = eval(request.params.opdracht);
+    response.send(String(uitkomst));
 })
 
-console.log('done')
+console.log('done') 
 
 app.listen(4567);
